ab5ba71652973ae0ebbe5380775aa9f1e4645e7f
Michelle Mukami <kamimello463@gmail.com>
2019-09-11 11:38:20 +0300

    app portfolio

--- kami/settings.py
+++ kami/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'portfolio.apps.PortfoiloConfig',
+  
     'bootstrap3',   
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio.apps.PortfoiloConfig',
 ]
 
 MIDDLEWARE = [
