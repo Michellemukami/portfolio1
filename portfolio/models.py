19c5778ca650aafaffc704ff367afb472e9161b1
Michelle Mukami <kamimello463@gmail.com>
2019-09-11 11:20:55 +0300

    made project madel

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=60)
+    post = models.TextField(max_length=60)
+    project_image = models.ImageField(upload_to='project/', blank=True)
+    url= models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
+    def save_Project(self):
+        self.save()
