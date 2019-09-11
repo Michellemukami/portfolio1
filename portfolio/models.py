b286ae882497d1493e589e457159f39e40135c8e
Michelle Mukami <kamimello463@gmail.com>
2019-09-11 11:22:39 +0300

    made project madel

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=60)
     post = models.TextField(max_length=60)
-    project_image = models.ImageField(upload_to='project/', blank=True)
+    project_image = models.ImageField(upload_to='projects/', blank=True)
     url= models.CharField(max_length=100)
     def __str__(self):
         return self.title
